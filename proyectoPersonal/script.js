8a252db033fe257cbce5d331de69c53e0667cdbf
AlejandroGG96 <alejandrogimeno96@gmail.com>
2024-02-29 07:03:19 +0100

    Actualizacion

--- proyectoPersonal/script.js
+++
@@ -1,7 +0,0 @@
-let nombreUsuario;
-let botonLogin = document.getElementById("btn-login");
-const section1 = document.querySelector("bg-1");
-botonLogin.addEventListener('click',()=> {nombreUsuario = prompt("Como nos Dirigimos a ti?");
-alert(`Bienvenid@ ${nombreUsuario}, ya puedes entrar.`)});
-
-
